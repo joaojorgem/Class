d55012c4182ffb6f2f03d5209adc384269183c8d
joaojorgem <joaojorgepatos@gmail.com>
2021-03-02 10:27:30 +0000

    change

--- index.js
+++ index.js
@@ -2,32 +2,21 @@ const http = require('http');
 const axios = require('axios');
 
 http.createServer((req, res)=>{
-  res.write(user.join("\n")); // write a response
+  res.write(users.join(", ")); //display the list of users on the page
+  res.write("\n\n"+emails.join(", ")); //display the list of users on the page
   res.end(); //end the response
 }).listen(8000); // listen for requests on port 8000
 
-/**let users = []; // names of users will be stored here
-    axios.get("https://jsonplaceholder.typicode.com/users")
-    .then(({ data }) => {
-      users = data.map(user => user.email); // get only the names of the users and store in an array
-      console.log(users) 
-    })
-    .catch(error=>{
-      console.log(error);
-    });             
-    Syncs function**/  
-
-
 let users = []; // names of users will be stored here
-(async function getNames(){    
+let email = [];
+(async function getNames(){
   try{
     const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
     users = data.map(user=>user.name);
-    emails = data.map(user=>user.email);
-    console.log(users)
+    emails = data.map(email=>email.email);
+    console.log(users);
+    console.log(emails);
   } catch(error){
     console.log(error)
   }
 })()
-
-
